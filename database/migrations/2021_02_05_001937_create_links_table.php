ef34177efefba69e7254353c1752e535a7808348
KHaritov Andrei <avxodiar@gmail.com>
2021-02-05 00:53:09 +0300

    Миграции бд

---
+++ database/migrations/2021_02_05_001937_create_links_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('link', 1024);
+            $table->bigInteger('site_id')->unsigned();
+
+            $table->foreign('site_id')->references('id')->on('sites')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
---
+++ database/migrations/2021_02_05_001956_create_clicks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClicksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clicks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('time');
+            $table->smallInteger('width')->unsigned();
+            $table->smallInteger('x')->unsigned();
+            $table->smallInteger('y')->unsigned();
+            $table->bigInteger('link_id')->unsigned();
+
+            $table->foreign('link_id')->references('id')->on('links')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clicks');
+    }
+}
