53b4fac745b2311cc10849403a601c2c94aceb30
KHaritov Andrei <avxodiar@gmail.com>
2021-02-05 11:23:38 +0300

    Сохранение полученных данных в бд с использованием моделей

--- app/Http/Controllers/ClickResource.php
+++ app/Http/Controllers/ClickResource.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Click;
+use App\Link;
+use App\Site;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -10,7 +13,7 @@ class ClickResource extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function __invoke(Request $request)
@@ -18,32 +21,49 @@ class ClickResource extends Controller
         $request = $request->all();
 
         $link = $request['l'];
+        $clicks = $request['c'];
 
         // проверка обязательных параметров - ссылки и данных по кликам
-        if (!empty($link) && count($request['c'])) {
+        if (!empty($link) && count($clicks)) {
             $width = $request['w'];
             $timeOffset = $request['o'];
 
-            $data = [];
-            foreach ($request['c'] as $click) {
-                if (!empty($click)) {
-                    $data[] = [
-                        'time' => $timeOffset + $click['t'],
+            $dataClicks = [];
+            foreach ($clicks as $click) {
+                if (
+                    !empty($click['t'])
+                    && !empty($click['x'])
+                    && !empty($click['y'])
+                ) {
+                    $timestamp = (int) ($timeOffset + $click['t']) / 1000;
+                    $dataClicks[] = [
+                        'time' => date("Y-m-d H:i:s", $timestamp),
+                        'width' => $width,
                         'x' => $click['x'],
-                        'y' => $click['y']
+                        'y' => $click['y'],
                     ];
                 }
             }
 
             // если все данные в наличии, то сохраняем в БД
-            if(count($data)) {
+            if (count($dataClicks)) {
                 $url = parse_url($link);
 
-                // данные для сохранения
-                $query1 = $url['host'];
-                $query2 = [$url['path'], $width];
-                $query3 = $data;
+                // сохраняем имя сайта
+                $site = Site::firstOrCreate([ 'name' => $url['host'] ]);
 
+                // сохраняем ссылку(URN)
+                $link = Link::firstOrNew([ 'link' => $url['path'] ]);
+                if(!$link->exists) {
+                    $site->links()->save($link);
+                }
+
+                // сохраняем клики
+                $clickList = [];
+                foreach ($dataClicks as $dataClick) {
+                    $clickList[] = (new Click)->fill($dataClick);
+                }
+                $link->clicks()->saveMany($clickList);
             }
         }
     }
